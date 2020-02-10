382b1dcb35879b87c94a347b7270129a42d849be
Ivankov <ivan20481@gmail.com>
2020-02-10 10:25:35 +0300

    add e2e-test

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, onWelcomeButtonClick} = props;
 
   return (
     <section className="welcome">
@@ -11,7 +11,9 @@ const WelcomeScreen = (props) => {
           src="img/melody-logo.png"
           alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button">
+      <button
+        onClick={onWelcomeButtonClick}
+        className="welcome__button">
         <span className="visually-hidden">Начать игру</span>
       </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
@@ -26,7 +28,8 @@ const WelcomeScreen = (props) => {
 };
 
 WelcomeScreen.propTypes = {
-  errorsCount: PropTypes.number.isRequired
+  errorsCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired
 };
 
 export default WelcomeScreen;
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -5,7 +5,9 @@ import WelcomeScreen from "./welcome-screen.jsx";
 it(`Should WelcomeScreen render correctly`, () => {
   const tree = renderer
     .create(<WelcomeScreen
-      errorsCount={3}/>)
+      errorsCount={3}
+      onWelcomeButtonClick={() => {}}
+    />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
