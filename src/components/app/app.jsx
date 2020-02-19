02c78fa8c657e83a0a482b2b3d72fcba48be2468
Ivankov <ivan20481@gmail.com>
2020-02-19 17:20:35 +0300

    add game-screen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import ArtistQuestionScreen
   from "../artist-question-screen/artist-question-screen.jsx";
+import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen
   from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
@@ -38,24 +39,28 @@ class App extends PureComponent {
       switch (question.type) {
         case GameType.ARTIST:
           return (
-            <ArtistQuestionScreen question={question}
-              onAnswer={() => {
-                this.setState((prevState) => ({
-                  step: prevState.step + 1
-                }));
-              }}
-            />
+            <GameScreen type={question.type}>
+              <ArtistQuestionScreen question={question}
+                onAnswer={() => {
+                  this.setState((prevState) => ({
+                    step: prevState.step + 1
+                  }));
+                }}
+              />
+            </GameScreen>
           );
         case GameType.GENRE:
         default:
           return (
-            <GenreQuestionScreen question={question}
-              onAnswer={() => {
-                this.setState((prevState) => ({
-                  step: prevState.step + 1
-                }));
-              }}
-            />
+            <GameScreen type={question.type}>
+              <GenreQuestionScreen question={question}
+                onAnswer={() => {
+                  this.setState((prevState) => ({
+                    step: prevState.step + 1
+                  }));
+                }}
+              />
+            </GameScreen>
           );
       }
     }
