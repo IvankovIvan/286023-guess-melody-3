c69bfaaf9fef88654ba64c61f6cc3502215fdffd
Ivankov <ivan20481@gmail.com>
2020-02-19 21:17:13 +0300

    add HOC

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,6 +8,10 @@ import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen
   from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
+import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player.js";
+
+const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
+const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
 
 class App extends PureComponent {
   constructor(props) {
@@ -40,7 +44,7 @@ class App extends PureComponent {
         case GameType.ARTIST:
           return (
             <GameScreen type={question.type}>
-              <ArtistQuestionScreen question={question}
+              <ArtistQuestionScreenWrapped question={question}
                 onAnswer={() => {
                   this.setState((prevState) => ({
                     step: prevState.step + 1
@@ -53,7 +57,7 @@ class App extends PureComponent {
         default:
           return (
             <GameScreen type={question.type}>
-              <GenreQuestionScreen question={question}
+              <GenreQuestionScreenWrapped question={question}
                 onAnswer={() => {
                   this.setState((prevState) => ({
                     step: prevState.step + 1
@@ -77,13 +81,13 @@ class App extends PureComponent {
             {this._renderGameScreen()}
           </Route>
           <Route exact path="/artist">
-            <ArtistQuestionScreen
+            <ArtistQuestionScreenWrapped
               question={questions[1]}
               onAnswer={() => {}}
             />
           </Route>
           <Route exact path="/genre">
-            <GenreQuestionScreen
+            <GenreQuestionScreenWrapped
               question={questions[0]}
               onAnswer={() => {}}
             />
--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,20 +1,10 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
-import AudioPlayer from "../audio-player/audio-player.jsx";
 
 class ArtistQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isPlaying: true
-    };
-  }
-
   render() {
-    const {isPlaying} = this.state;
-    const {onAnswer, question} = this.props;
+    const {onAnswer, question, renderPlayer} = this.props;
     const {answers, song} = question;
 
     return (
@@ -22,9 +12,7 @@ class ArtistQuestionScreen extends PureComponent {
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
           <div className="track">
-            <AudioPlayer src={song.src} isPlaying={isPlaying}
-              onPlayButtonClick={() => this.setState({isPlaying: !isPlaying})}
-            />
+            {renderPlayer(song.src, 0)}
           </div>
         </div>
 
@@ -64,7 +52,8 @@ ArtistQuestionScreen.propTypes = {
       src: PropTypes.string.isRequired
     }).isRequired,
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired
-  }).isRequired
+  }).isRequired,
+  renderPlayer: PropTypes.func.isRequired
 };
 
 export default ArtistQuestionScreen;
